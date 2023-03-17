44763e310bd56378aebcd73802eb66e36d25bff3
YeduKrishnan A <kuttuzz17@gmail.com>
2023-03-17 15:31:05 +0530

    Add UI and Functionality: Win and Restart Game

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,96 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Die from "./Pages/Die"
 import Heading from "./Pages/Heading"
 
+import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
 
 export default function App() {
 
-    const [dice, setDice] = useState(allNewDice)
+    const [tenzies, setTenzies] = useState(false);
+    const [dice, setDice] = useState(allNewDice);
 
-    function allNewDice() {
+    useEffect(() => {
+        // console.log('Dice State Changed')
+
+        const allTrue = dice.every(dices => dices.isHeld) //check if every dice is true
+        const firstValue = dice[0].value;
+        const sameValue = dice.every(dices => dices.value === firstValue)
+
+        if (allTrue && sameValue) {
+            setTenzies(true);
+        }
+
+    }, [dice])
+
+    function allNewDice() { // add 10 random value to an array
+
         const arr = []
         for (let i = 0; i < 10; i++) {
-            arr.push(Math.ceil(Math.random() * 6))
+            arr.push(generateNewDice())
         }
         return (arr)
     }
 
-    const diceElement = dice.map((dices) => {
+    function generateNewDice() {
+        return (
+            {
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            }
+        )
+    }
+
+    const eachDiceElement = dice.map((dices) => {
         return (
             <Die
-                value={dices}
+                key={dices.id}
+                value={dices.value}
+                isHeld={dices.isHeld}
+                onClick={() => holdDice(dices.id)}
+            // id={dices.id}
+            // onClick={holdDice}
             />
         )
     })
 
-    function rollDiceClick() {
-        setDice(allNewDice);
+
+    function holdDice(id) {
+        setDice(prevDice => {
+            return (
+                prevDice.map((item) => {
+                    return (
+                        item.id === id ?
+                            { ...item, isHeld: !item.isHeld } :
+                            item
+                    )
+                })
+            )
+        })
+    }
+
+
+
+    function rollDiceHandle() {
+        if (tenzies === false) {  //if player didn't win, roll dice
+            setDice(prevDice =>
+                prevDice.map((item) => item.isHeld === true ? { ...item } : generateNewDice())
+            )
+        }
+        else { // if player wins 
+            setTenzies(false);
+            setDice(allNewDice)
+        }
     }
 
     return (
         <main>
+            {tenzies && < Confetti />}
             <div className="sike">
                 <Heading />
                 <div className="dieContainer">
-                    {diceElement}
+                    {eachDiceElement}
                     {/* <Die value={newDice[0]} />
                     <Die value={newDice[1]} />
                     <Die value={newDice[2]} />
@@ -45,7 +103,7 @@ export default function App() {
                     <Die value={newDice[9]} /> */}
                 </div>
                 <div className="rollDiceDiv">
-                    <button onClick={rollDiceClick} className="rollDice">Roll Dice</button>
+                    {<button onClick={rollDiceHandle} className="rollDice">{tenzies ? "New Game" : "Roll"}</button>}
                 </div>
             </div>
         </main>
--- src/Pages/Die.jsx
+++ src/Pages/Die.jsx
@@ -1,5 +1,10 @@
 export default function Die(props) {
+    let color;
+    const styles = {
+        backgroundColor: props.isHeld === true ? "#59E391" : "white"
+    }
     return (
-        <div className="dieEach">{props.value}</div>
+        // <div onClick={() => props.onClick(props.id)} style={styles} className="dieEach">{props.value}</div >
+        <div onClick={props.onClick} style={styles} className="dieEach">{props.value}</div >
     )
 }
